7f823c78266398cdc8b8adb2c43f8e7211d6224b
dgrajewski <dawids115@gmail.com>
2019-03-12 19:33:14 +0100

    Dodano system zmiany aktywności; Dodano aktywnośc gry;

--- app/src/main/java/com/adt/game_of_life/GameOfLifeApplication.kt
+++ app/src/main/java/com/adt/game_of_life/GameOfLifeApplication.kt
@@ -1,6 +1,7 @@
 package com.adt.game_of_life
 
 import android.app.Application
+import com.adt.game_of_life.viewmodel.GameViewModel
 import com.adt.game_of_life.viewmodel.MenuViewModel
 import org.koin.android.ext.android.startKoin
 import org.koin.android.viewmodel.ext.koin.viewModel
@@ -19,6 +20,7 @@ class GameOfLifeApplication : Application() {
     private fun getApplicationModule(): Module {
         return module {
             viewModel { MenuViewModel() }
+            viewModel { GameViewModel() }
         }
     }
 }
--- app/src/main/java/com/adt/game_of_life/viewmodel/MenuViewModel.kt
+++ app/src/main/java/com/adt/game_of_life/viewmodel/MenuViewModel.kt
@@ -1,7 +1,16 @@
 package com.adt.game_of_life.viewmodel
 
 import android.arch.lifecycle.ViewModel
+import android.os.Bundle
+import com.adt.game_of_life.view.activity.GameActivity
+import pl.grajek.actions.model.SingleLiveEvent
+import pl.grajek.actions.model.dto.ActivityStartModel
 
 class MenuViewModel : ViewModel() {
 
+    val activityToStart = SingleLiveEvent<ActivityStartModel>()
+
+    fun startGameActivity() {
+        activityToStart.value = ActivityStartModel(GameActivity::class.java, Bundle())
+    }
 }
