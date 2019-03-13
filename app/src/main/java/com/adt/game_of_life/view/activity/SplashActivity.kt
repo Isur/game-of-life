2482a2b8f255cc2e00d6778ea6e74192f8ff6d13
dgrajewski <dawids115@gmail.com>
2019-03-13 20:33:07 +0100

    Dodanie biblioteki do wyświetlania gifów; Zwiększenie minSdk do 17; Wyświetlanie gifa jako splash screen;

--- app/src/main/java/com/adt/game_of_life/view/activity/SplashActivity.kt
+++ app/src/main/java/com/adt/game_of_life/view/activity/SplashActivity.kt
@@ -6,15 +6,28 @@ import android.support.v7.app.AppCompatActivity
 import com.adt.game_of_life.R
 import com.adt.game_of_life.model.dto.ActivityStartModel
 import com.adt.game_of_life.util.startActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+import pl.droidsonroids.gif.GifDrawable
 
 class SplashActivity : AppCompatActivity() {
 
-    private val visibilityTime = 2000L
+    private val visibilityTime = 3000L
+    private val splashGifUrl = "gif/splash.gif"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        setupSplashGif()
+        setupTransition()
+    }
+
+    private fun setupSplashGif() {
+        val gifDrawable = GifDrawable(assets, splashGifUrl)
+        splashImageView.background = gifDrawable
+    }
+
+    private fun setupTransition() {
         Handler().postDelayed({
             val model = ActivityStartModel(MenuActivity::class.java, Bundle())
             startActivity(model)
