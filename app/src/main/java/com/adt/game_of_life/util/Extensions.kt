4cf28a4ab634d92a20e8c8c51ed48c8633657010
dawids222 <dawids115@gmail.com>
2019-03-19 20:34:44 +0100

    Settings (#5)
    * Podstawowy wygląd widoku ustawień;
    
    * Dodano struktury do zapisywania ustawień gry;
    
    * Dodano struktury do zapisywania colorów gry;
    
    * Dodanie modeli do zarządzanmia ustawieniami;
    
    * Połączenie widoku z logiką;
    
    * Naprawa buga ze złym odtwarzaniem stanu checkboxów;
    
    * Bugfix elewacji druiego color buttona;
    
    * Zoptymalizowano wstrzykiwanie shared prefs;
    
    * Zmiana wizualna aktywności ustawień;
    
    * Zamiana pozostałych literałów na zasoby;

--- app/src/main/java/com/adt/game_of_life/util/Extensions.kt
+++ app/src/main/java/com/adt/game_of_life/util/Extensions.kt
@@ -1,11 +1,18 @@
 package com.adt.game_of_life.util
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import com.adt.game_of_life.R
 import com.adt.game_of_life.model.dto.ActivityStartModel
+import com.flask.colorpicker.ColorPickerView
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder
+import timber.log.Timber
 
 fun AppCompatActivity.startActivity(model: ActivityStartModel) {
     val intent = Intent(this, model.type)
@@ -15,4 +22,35 @@ fun AppCompatActivity.startActivity(model: ActivityStartModel) {
 
 fun <T : ViewDataBinding> AppCompatActivity.getBinding(activity: Activity, layout: Int): T {
     return DataBindingUtil.setContentView(activity, layout)
+}
+
+fun Button.showColorPicker(context: Context, initialColor: Int, callback: (Int) -> Unit) {
+    val header = context.getString(R.string.dialog_choose_color_header)
+    val ok = context.getString(R.string.dialog_ok_button)
+    val cancel = context.getString(R.string.dialog_cancel_button)
+
+    ColorPickerDialogBuilder
+        .with(context, R.style.defaultDialogStyle)
+        .setTitle(header)
+        .initialColor(initialColor)
+        .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+        .density(12)
+        .setOnColorSelectedListener { selectedColor ->
+            Timber.e(selectedColor.toString())
+        }
+        .setPositiveButton(
+            ok
+        ) { _, selectedColor, _ ->
+            this.setBackgroundColor(selectedColor)
+            callback(selectedColor)
+        }
+        .setNegativeButton(cancel) { _, _ -> }
+        .build()
+        .show()
+}
+
+fun SharedPreferences.edit(callback: (SharedPreferences.Editor) -> Unit) {
+    val editor = this.edit()
+    callback(editor)
+    editor.commit()
 }
