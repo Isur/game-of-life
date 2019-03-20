676a953859c2cd7f91d65aff309877037845c5c7
dgrajewski <dawids115@gmail.com>
2019-03-20 20:24:00 +0100

    Czyszczenie kodu przed pullem;

--- app/src/main/java/com/adt/game_of_life/view/activity/SplashActivity.kt
+++ app/src/main/java/com/adt/game_of_life/view/activity/SplashActivity.kt
@@ -6,25 +6,16 @@ import android.support.v7.app.AppCompatActivity
 import com.adt.game_of_life.R
 import com.adt.game_of_life.model.dto.ActivityStartModel
 import com.adt.game_of_life.util.startActivity
-import kotlinx.android.synthetic.main.activity_splash.*
-import pl.droidsonroids.gif.GifDrawable
 
 class SplashActivity : AppCompatActivity() {
 
     private val visibilityTime = 2000L
-    private val splashGifUrl = "gif/splash.gif"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        //setupSplashGif()
         setupTransition()
-    }
-
-    private fun setupSplashGif() {
-        val gifDrawable = GifDrawable(assets, splashGifUrl)
-        splashImageView.background = gifDrawable
     }
 
     private fun setupTransition() {
