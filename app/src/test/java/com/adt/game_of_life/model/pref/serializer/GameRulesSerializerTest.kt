f280cf951390c6a427d549d669e552f0a1fb8947
Isur <bednarczykartur96@gmail.com>
2019-05-05 17:30:43 +0200

    Unit test - GameRuleSerializer

--- app/src/test/java/com/adt/game_of_life/model/pref/serializer/GameRulesSerializerTest.kt
+++ app/src/test/java/com/adt/game_of_life/model/pref/serializer/GameRulesSerializerTest.kt
@@ -6,10 +6,9 @@ import org.junit.Assert.*
 
 class GameRulesSerializerTest {
     private val serializer = GameRulesSerializer()
-
     @Test
     fun serialize() {
-        val toSerialize = listOf(1,2,3)
+        val toSerialize = listOf(1, 2, 3)
         val expected = "1;2;3;"
         val result = serializer.serialize(toSerialize)
         assertEquals(expected, result)
@@ -18,38 +17,22 @@ class GameRulesSerializerTest {
     @Test
     fun deserialize() {
         val toDeserialize = "4;5;6;"
-        val expected = listOf(4,5,6)
+        val expected = listOf(4, 5, 6)
         val result = serializer.deserialize(toDeserialize)
         assertEquals(expected, result)
     }
 
     @Test
-    fun deserializeNotNumbers(){
-        val toDeserialize = "Just some test string."
+    fun deserializeNotNumber() {
+        val toDeserialize = "String test"
         assertFails {
             serializer.deserialize(toDeserialize)
         }
     }
 
     @Test
-    fun deserializeIncorrectString(){
-        val toDeserialize = "2 2 3 "
-        assertFails {
-            serializer.deserialize(toDeserialize)
-        }
-    }
-
-    @Test
-    fun serializeBiggerThanDigit(){
-        val toSerialize = listOf(12,32,45)
-        assertFails {
-            serializer.serialize(toSerialize)
-        }
-    }
-
-    @Test
-    fun deserializeBiggerThanDigit(){
-        val toDeserialize = "123;2312;42"
+    fun deserializeIncorrectString() {
+        val toDeserialize = "2 2 3   "
         assertFails {
             serializer.deserialize(toDeserialize)
         }
