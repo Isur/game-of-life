3ad491b7152e54b24be5f0aeb727e93fe8d4d36d
Artur Bednarczyk <bednarczykartur96@gmail.com>
2019-05-19 14:14:44 +0200

    Version 1.1 (#19)
    * Zamiana ikony aplikacji; (#16)
    * Board-size fix after loading (#18)
    * Documentaion removed

--- app/src/main/java/com/adt/game_of_life/viewmodel/LoadViewModel.kt
+++ app/src/main/java/com/adt/game_of_life/viewmodel/LoadViewModel.kt
@@ -6,11 +6,13 @@ import com.adt.game_of_life.R
 import com.adt.game_of_life.model.algorithm.IBoardManipulator
 import com.adt.game_of_life.model.event.SingleLiveEvent
 import com.adt.game_of_life.model.file.IFileManager
+import com.adt.game_of_life.model.pref.IBoardPref
 import com.adt.game_of_life.model.snackbar.SnackBarModel
 
 class LoadViewModel(
     private val boardManipulator: IBoardManipulator,
-    private val fileManager: IFileManager
+    private val fileManager: IFileManager,
+    private val boardPref: IBoardPref
 ) : ViewModel() {
 
     val files = MutableLiveData<List<String>>()
@@ -23,6 +25,10 @@ class LoadViewModel(
     fun load(filename: String) {
         val loaded = fileManager.getContent(filename)
         boardManipulator.setBoard(loaded)
+      
+        boardPref.setHeight(loaded.size)
+        boardPref.setWidth(loaded[0].size)
+      
         snackBar.value = SnackBarModel(R.string.save_loaded, R.color.success)
     }
 
